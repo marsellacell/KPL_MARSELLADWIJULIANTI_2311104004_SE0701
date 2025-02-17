4b70a92ec305bfe0f16bcb673fe5bebe67365ba0
marsella-dwi-julianti <marselladwijulianti@gmail.com>
2025-02-17 16:42:52 +0700

    pertemuan 1

---
+++ 01_Pengantar_Praktikum_KPL/challenge.js
@@ -0,0 +1,18 @@
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+input.question("Siapa nama kamu? ", (nama) => {
+    input.question("Jurusan kamu? ", (jurusan) => {
+        input.question("Universitas kamu? ", (universitas) => {
+            input.question("Umur kamu? ", (umur) => {
+                console.info(`Hallo ${nama}, jurusan kamu adalah ${jurusan}, kamu kuliah di ${universitas}, dan umur kamu ${umur} tahun.`);
+                input.close();
+            });
+        });
+    });
+});
