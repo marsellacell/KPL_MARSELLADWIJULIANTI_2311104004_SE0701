a5018bd996d7459fbdb463cac4afa07bac4c0ef1
marsella-dwi-julianti <marselladwijulianti@gmail.com>
2025-02-24 12:12:04 +0700

    Praktikum02

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variabels.js
@@ -0,0 +1,23 @@
+var name = "Marsella Dwi Julianti"
+const nim = 2311104004
+
+console.log(name)
+
+console.log("nama saya adalah " + nim)
+console.log(`Nama saya adalah ${name} dan nim saya adalah ${nim}`)
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`)
+
+var name = "ayam"
+
+let fullName = "Marsella Dwi Julianti"
+
+//const kelas
+//nim = 2311104004;
+
+/*
+hdhjshadkf
+
+sjhdjdjd
+*/
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJS/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVariable = "Marsella Dwi Julianti"
+
+function getMyIdentity() {
+    let localVariable = 2311104004
+
+    console.log(`Nama saya ${globalVariable} nim ${localVariable}`)
+}
+
+console.log(globalVariable)
+// console.log(localVariable)
+
+getMyIdentity()
