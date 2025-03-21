832e19e06536899622c8085e337585fe073c90e7
marsella-dwi-julianti <marselladwijulianti@gmail.com>
2025-03-21 22:02:34 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ 05_generics_dbc/TUGAS PENDAHULUAN/script.js
@@ -0,0 +1,8 @@
+class HaloGeneric {
+    sapaUser(user) {
+        console.log(`Halo user ${user}`);
+    }
+}
+
+const halo = new HaloGeneric();
+halo.sapaUser("Marsell"); // Ganti dengan nama panggilan praktikan
